124bec8ae11a733af62fc12821b72f3817758e8a
panchenko-kate <kmaslova_007@ukr.net>
2023-01-26 20:35:37 +0200

    add lightbox import

--- src/js/gallery-makup.js
+++ src/js/gallery-makup.js
@@ -1,5 +1,5 @@
 // import SimpleLightbox from "Simplelightbox";
-// import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery');
